5d833abc91b21f107f1d61d315558a031be24be9
Tofupaste <abiputra3566@gmail.com>
2024-10-05 23:57:32 +0700

    update

--- oop_class.dart
+++ oop_class.dart
@@ -1,3 +1,27 @@
-void main () {
-  
+// Object oriented class in dart
+
+class Person {
+  String name;
+  int age;
+
+  // Constructor
+  Person(this.name, this.age);
+
+  // Method
+  void showName() {
+    print("Name: ${this.name}");
+  }
+
+  void showAge() {
+    print("Age: ${this.age}");
+  }
+}
+
+void main() {
+  // Creating object of class
+  Person person = new Person("Shiho", 16);
+
+  // Accessing properties
+  person.showName();
+  person.showAge();
 }
