ec6ea46798d2f9429a10ca6908192f53e6b3defc
Tofupaste <abiputra3566@gmail.com>
2024-10-09 01:55:37 +0700

    main

---
+++ opp_setter_getter/main.dart
@@ -0,0 +1,10 @@
+import 'sub.dart';
+
+void main() {
+  // Creating an instance of Zutomayo with all the required parameters
+  var zutomayo = Zutomayo('Acane', 25, 'Ketto Bashitamoufu', 'Some Favorite Song');
+  
+  // Calling methods to display the information
+  zutomayo.havetype();
+  zutomayo.ilke();
+}
